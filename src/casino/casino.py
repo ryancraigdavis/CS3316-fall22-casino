a4120c4c0c1b1d611c3af799be6e1fc625499e8d
Ryan Davis <ryandavis@deloitte.com>
2022-09-27 14:32:43 -0700

    Initial Casino Commit

---
+++ src/casino/casino.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A set of various casino games as classes"""
+import os
+import random
+
+
+class HelloCasino:
+    """Welcome to the casino"""
+
+    def __init__(self):
+        """Inits the players name"""
+        self.name = "Joe"
+
+    def welcome(self):
+        """Welcome shouter"""
+        print(f"Welcome to the Casino, {self.name}")
+
+# Run main here.
+if __name__ == "__main__":
+    NewCasino = HelloCasino()
+    NewCasino.welcome()
