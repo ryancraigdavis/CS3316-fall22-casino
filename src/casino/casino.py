2ad7c9c2dc807c2624a29f841c4a5e0a18a80d1d
Tim Lybeck <lybecktim@gmail.com>
2022-09-29 14:08:39 -0600

    added simple game selector

--- src/casino/casino.py
+++ src/casino/casino.py
@@ -14,8 +14,20 @@ class HelloCasino:
     def welcome(self):
         """Welcome shouter"""
         print(f"Welcome to the Casino, {self.name}")
+    def selectGame(self):
+        while(True):
+            print("Games:\nteam1Game:1\nteam2game:2\nteam3game:3\nBlackjack:4\nteam5game:5\nQuit:q")
+            selectedGame = input("What game would you like to play?")
+            print(selectedGame)
+            if(selectedGame == "4"):
+                print("Blackjack selected")
+            elif(selectedGame.lower() == "q"):
+                break
+            else:
+                print("invalid selection")
 
 # Run main here.
 if __name__ == "__main__":
     NewCasino = HelloCasino()
     NewCasino.welcome()
+    NewCasino.selectGame()
