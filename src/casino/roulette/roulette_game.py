e1ff5e05d1b54739debb1877695e40c7bbcde69a
Kyle Nielsen <kylenielsen2@isu.edu>
2022-10-18 17:34:18 -0600

    This is the code after the refactoring

--- src/casino/roulette/roulette_game.py
+++ src/casino/roulette/roulette_game.py
@@ -22,6 +22,9 @@ if __name__ == "__main__":
     NewCasino.welcome()
 
 
+
+
+
 class playRoulette:
     red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
     black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
@@ -30,7 +33,6 @@ class playRoulette:
     even = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
     front_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
     back_nums = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
-    zero = [0]
     print(
         "Welcome to the roulette tables! Please enter some starting information to begin."
     )
@@ -61,35 +63,23 @@ class playRoulette:
     def chooseBet(self):
         startBet = True
         while startBet:
-            betSelection = str(
+            betSelection = int(
                 input(
-                    "What number would you like to bet on?\n\t(N) Single Number\n\t(H) Front half or Back half\n\t(O) Odd or Even\n\t(R) Red or Black\n"
+                    "What number would you like to bet on?\n\t(1) Single Number\n\t(2) Front half\n\t(3) Back half\n\t(4) Odd\n\t(5) Even\n\t(6) Red\n\t(7) Black\n"
                 )
             )
-            if betSelection.upper() == "N":
-                self.betType = "N"
-                return self.betType
-            elif betSelection.upper() == "H":
-                self.betType = "H"
-                return self.betType
-            elif betSelection.upper() == "O":
-                self.betType = "O"
-                return self.betType
-            elif betSelection.upper() == "R":
-                self.betType = "R"
-                return self.betType
+            if 8 > betSelection > 0:
+                return betSelection
             else:
                 print("That is not a valid selection.")
 
     # actually plays the game and uses the variable set in chooseBet to compare to actual outcome
-    def playGame(
-        self,
-    ):  # having an issue where bet types are overriding the check and making certain bets not calculate correctly
+    def playGame(self,):  # having an issue where bet types are overriding the check and making certain bets not calculate correctly
         keepPlaying = True
         while (
             keepPlaying
         ):  # sets game to be continuous until player says to stop playing
-            if self.playerBalance > 0:  # checks player has any money to play with
+            while self.playerBalance > 0:  # checks player has any money to play with
                 # Menu for player to decide what to do
                 whatToDo = input(
                     "What would you like to do?\n\t(C) Check your current balance\n\t(P) Play roulette\n\t(Q) Quit the game\n"
@@ -100,26 +90,16 @@ class playRoulette:
                     )  # displays current balance
 
                 elif whatToDo.upper() == "P":  # starts playing the game
-                    betAmount = self.placeBet(
-                        self.playerBalance
-                    )  # gets bets from player
-                    bet = self.chooseBet()
-                    if bet == "N":
-                        self.singleNumberPlay(betAmount)
-                    elif bet == "H":
-                        self.isHalves(betAmount)
-                    elif bet == "O":
-                        self.oddOrEvenPlay(betAmount)
-                    elif bet == "R":
-                        self.redOrBlack(betAmount)
-
+                    betType = self.chooseBet() - 1 # gets the bet type
+                    betAmount = int(input("How much do you want to bet? ")) # gets the bet amount
+                    playRoulette.betList[betType](self,betAmount) # will check the index of a list of the function and use the bet amount given
                 elif whatToDo.upper() == "Q":  # exits game
                     keepPlaying = False
                 else:
                     print("That is not a valid selection.")
-            else:  # ends the game if player runs out of money
-                print("Sorry you are out of money.")
-                keepPlaying = False
+              # ends the game if player runs out of money
+            print("Sorry you are out of money.")
+            keepPlaying = False
 
     def singleNumberPlay(self, betAmount):
         userNumber = int(input("What number would you like to choose?"))
@@ -131,79 +111,67 @@ class playRoulette:
             self.loseText(spunNumber)
             self.playerBalance = self.playerBalance - betAmount
 
-    def oddOrEvenPlay(self, betAmount):
-        userSelection = input(
-            "Would you like to play odd or even?\n\t(D) Odd\n\t(E) Even\n"
-        )
+    def oddPlay(self, betAmount):
+        print("You have chosen odd.")
         spunNumber = self.spinTheWheel()
-        if userSelection.upper() == "D":
-            for num in self.odd:
-                if spunNumber == self.odd[num]:
-                    self.winText(spunNumber)
-                    self.playerBalance += betAmount * 2
-                    break
-                else:
-                    self.loseText(spunNumber)
-                    self.playerBalance -= betAmount
-                    break
+        for num in self.odd:
+            if spunNumber == self.odd[num]:
+                self.winText(spunNumber)
+                self.playerBalance += betAmount * 2
+                break
+            else:
+                self.loseText(spunNumber)
+                self.playerBalance -= betAmount
+                break
 
-        if userSelection.upper() == "E":
-            for num in self.even:
-                if spunNumber == self.even[num]:
-                    self.winText(spunNumber)
-                    self.playerBalance += betAmount * 2
-                    break
-                else:
-                    self.loseText(spunNumber)
-                    self.playerBalance -= betAmount
-                    break
+    def evenPlay(self, betAmount):
+        print("You have chosen even.")
+        spunNumber = self.spinTheWheel()
+        for num in self.even:
+            if spunNumber == self.even[num]:
+                self.winText(spunNumber)
+                self.playerBalance += betAmount * 2
+                break
+            else:
+                self.loseText(spunNumber)
+                self.playerBalance -= betAmount
+                break
+
+    def frontHalf(self, betAmount):
+        ballNum = self.spinTheWheel()
+        if ballNum in self.front_nums:
+                self.winText(ballNum)
+                self.playerBalance += betAmount
         else:
-            print("That is not a valid selection")
+            self.loseText(ballNum)
+            self.playerBalance -= betAmount
 
-    def isHalves(self, betAmount):
-        frontOrBack = input("Do you want to bet on the front or back half? (F/B) ")
-        if frontOrBack.upper() == "F":
-            whichHalf = "F"
-        elif frontOrBack.upper() == "B":
-            whichHalf = "B"
+    def backHalf(self, betAmount):
         ballNum = self.spinTheWheel()
-        print("The ball landed in " + str(ballNum))
-        print(whichHalf)
-        if whichHalf == "F":
-            if ballNum in self.front_nums:
+        if ballNum in self.back_nums:
                 self.winText(ballNum)
                 self.playerBalance += betAmount
-            else:
-                self.loseText(ballNum)
-                self.playerBalance -= betAmount
-        if whichHalf == "B":
-            print("You chose the back half.")
-            if ballNum in self.back_nums:
-                self.winText(ballNum)
-                self.playerBalance += betAmount
-            else:
-                self.loseText(ballNum)
-                self.playerBalance += betAmount
+        else:
+            self.loseText(ballNum)
+            self.playerBalance += betAmount
 
-    def redOrBlack(self, betAmount):
-        userColor = input("What color would you like to choose? black or red (X/Y)")
+    def redPlay(self, betAmount):
         ballNum = self.spinTheWheel()
-
-        if userColor.upper() == "Y":
-            if ballNum in self.red:
+        if ballNum in self.red:
                 self.winText(ballNum)
                 self.playerBalance += betAmount
-            else:
-                self.loseText(ballNum)
-                self.playerBalance -= betAmount
+        else:
+            self.loseText(ballNum)
+            self.playerBalance -= betAmount
 
-        if userColor.upper() == "X":
-            if ballNum in self.black:
+    def blackPlay(self, betAmount):
+        ballNum = self.spinTheWheel()
+        if ballNum in self.black:
                 self.winText(ballNum)
                 self.playerBalance += betAmount
-            else:
-                self.loseText(ballNum)
-                self.playerBalance -= betAmount
+        else:
+            self.loseText(ballNum)
+            self.playerBalance -= betAmount
 
     def winText(self, num):
         return print(
@@ -213,7 +181,12 @@ class playRoulette:
     def loseText(self, num):
         return print("\tYou lose! The number was " + str(num) + "!\n")
 
-
+    # this is a list of functions so that they can be called by index
+    # THIS HAS TO BE AFTER ALL FUNCTIONS ARE MADE
+    betList = [singleNumberPlay, frontHalf, backHalf, oddPlay, evenPlay, redPlay, blackPlay]
 # Driver code
-# class_instance = playRoulette()
-# class_instance.playGame()
+
+class_instance = playRoulette()
+class_instance.playGame()
+
+
