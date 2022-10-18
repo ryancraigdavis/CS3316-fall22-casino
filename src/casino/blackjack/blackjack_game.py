156339bbc35fc39d77c1378b6db4c1d3d95b1c64
Zackary Beck <beckzack@isu.edu>
2022-10-18 14:14:46 -0600

    refactored blackjack code

--- src/casino/blackjack/blackjack_game.py
+++ src/casino/blackjack/blackjack_game.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """A set of various casino games as classes"""
 import os
 import random
@@ -33,7 +32,7 @@ class HelloCasino:
 
 class BlackJack:
 
-    deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
+    deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"] * 4
 
     def __init__(self):
         BlackJack.game()
@@ -43,14 +42,6 @@ class BlackJack:
         for i in range(2):
             random.shuffle(deck)
             card = deck.pop()
-            if card == 11:
-                card = "J"
-            if card == 12:
-                card = "Q"
-            if card == 13:
-                card = "K"
-            if card == 14:
-                card = "A"
             hand.append(card)
         return hand
 
@@ -59,36 +50,33 @@ class BlackJack:
         if again == "y":
             dealer_hand = []
             player_hand = []
-            deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
+            deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"] * 4
             BlackJack.game()
         else:
             print("Bye!")
             exit()
 
+    @staticmethod
     def total(hand):
         total = 0
         for card in hand:
             if card == "J" or card == "Q" or card == "K":
                 total += 10
             elif card == "A":
-                if total >= 11:
-                    total += 1
-                else:
-                    total += 11
+                total += BlackJack.ace_card(total)
             else:
                 total += card
         return total
 
+    @staticmethod
+    def ace_card(total):
+        if total >= 11:
+            return 1
+        else:
+            return 11
+
     def hit(hand):
         card = BlackJack.deck.pop()
-        if card == 11:
-            card = "J"
-        if card == 12:
-            card = "Q"
-        if card == 13:
-            card = "K"
-        if card == 14:
-            card = "A"
         hand.append(card)
         return hand
 
@@ -123,26 +111,71 @@ class BlackJack:
             print("Sorry, you lose. The dealer got a blackjack.\n")
             BlackJack.play_again()
 
-    def score(dealer_hand, player_hand):
+    def check_player_wins(dealer_hand, player_hand):
         if BlackJack.total(player_hand) == 21:
             BlackJack.print_results(dealer_hand, player_hand)
             print("Congratulations! You got a Blackjack!\n")
-        elif BlackJack.total(dealer_hand) == 21:
+        else: pass
+
+    def check_dealer_wins(dealer_hand, player_hand):
+        if BlackJack.total(dealer_hand) == 21:
             BlackJack.print_results(dealer_hand, player_hand)
             print("Sorry, you lose. The dealer got a blackjack.\n")
-        elif BlackJack.total(player_hand) > 21:
+        else: pass
+
+    def check_player_loses(dealer_hand, player_hand):
+        if BlackJack.total(player_hand) > 21:
             BlackJack.print_results(dealer_hand, player_hand)
             print("Sorry. You busted. You lose.\n")
-        elif BlackJack.total(dealer_hand) > 21:
+        else: pass
+
+    def check_dealer_busts(dealer_hand, player_hand):
+        if BlackJack.total(dealer_hand) > 21:
             BlackJack.print_results(dealer_hand, player_hand)
             print("Dealer busts. You win!\n")
-        elif BlackJack.total(player_hand) < BlackJack.total(dealer_hand):
-            BlackJack.print_results(dealer_hand, player_hand)
-        # print ("Sorry. Your score isn't higher than the dealer. You lose.\n")
-        elif BlackJack.total(player_hand) > BlackJack.total(dealer_hand):
+        else: pass
+
+    def check_losing_score(dealer_hand, player_hand):
+        if BlackJack.total(player_hand) < BlackJack.total(dealer_hand):
+           BlackJack.print_results(dealer_hand, player_hand)
+           print ("Sorry. Your score isn't higher than the dealer. You lose.\n")
+        else: pass
+
+    def check_winning_score(player_hand, dealer_hand):
+        if BlackJack.total(player_hand) > BlackJack.total(dealer_hand):
             BlackJack.print_results(dealer_hand, player_hand)
             print("Congratulations. Your score is higher than the dealer. You win\n")
+        else: pass
 
+    def score(dealer_hand, player_hand):
+        BlackJack.check_player_wins(dealer_hand,player_hand)
+        BlackJack.check_dealer_wins(dealer_hand,player_hand)
+        BlackJack.check_player_loses(dealer_hand, player_hand)
+        BlackJack.check_dealer_busts(dealer_hand, player_hand)
+        BlackJack.check_losing_score(dealer_hand, player_hand)
+        BlackJack.check_winning_score(dealer_hand, player_hand)
+
+    @staticmethod
+    def chose_h(player_hand,dealer_hand):
+        BlackJack.hit(player_hand)
+        while BlackJack.total(dealer_hand) < 17:
+            BlackJack.hit(dealer_hand)
+        BlackJack.score(dealer_hand, player_hand)
+        BlackJack.play_again()
+
+    @staticmethod
+    def chose_s(player_hand,dealer_hand):
+        while BlackJack.total(dealer_hand) < 17:
+            BlackJack.hit(dealer_hand)
+        BlackJack.score(dealer_hand, player_hand)
+        BlackJack.play_again()
+
+    @staticmethod
+    def chose_q():
+        print("Bye!")
+        exit()
+
+    @staticmethod
     def game():
         choice = 0
         BlackJack.clear()
@@ -151,29 +184,16 @@ class BlackJack:
         player_hand = BlackJack.deal(BlackJack.deck)
         while choice != "q":
             print("The dealer is showing a " + str(dealer_hand[0]))
-            print(
-                "You have a "
-                + str(player_hand)
-                + " for a total of "
-                + str(BlackJack.total(player_hand))
-            )
+            print("You have a " + str(player_hand) + " for a total of " + str(BlackJack.total(player_hand)))
             BlackJack.blackjack(dealer_hand, player_hand)
             choice = input("Do you want to [H]it, [S]tand, or [Q]uit: ").lower()
             BlackJack.clear()
             if choice == "h":
-                BlackJack.hit(player_hand)
-                while BlackJack.total(dealer_hand) < 17:
-                    BlackJack.hit(dealer_hand)
-                BlackJack.score(dealer_hand, player_hand)
-                BlackJack.play_again()
+                BlackJack.chose_h(player_hand,dealer_hand)
             elif choice == "s":
-                while BlackJack.total(dealer_hand) < 17:
-                    BlackJack.hit(dealer_hand)
-                BlackJack.score(dealer_hand, player_hand)
-                BlackJack.play_again()
+                BlackJack.chose_s(player_hand,dealer_hand)
             elif choice == "q":
-                print("Bye!")
-                exit()
+                BlackJack.chose_q()
 
 
 # Run main here.
